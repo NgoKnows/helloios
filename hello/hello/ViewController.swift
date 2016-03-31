55ab8822c2a4158167f8620f247eaefeb411462d
Alex <alex_ngo@live.com>
2016-03-30 23:38:04 -0700

    fixed problem

---
+++ hello/hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by Alex Ngo on 3/30/16.
+//  Copyright © 2016 Alex Ngo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
